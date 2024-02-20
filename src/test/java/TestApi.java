a8982998c3289bc051943f09c9e5f756d9369c57
Tobias Correa Camilo <thobias2501@gmail.com>
2024-02-20 15:57:50 -0300

    refactor rename is name class

--- src/test/java/TestApi.java
+++ src/test/java/TestApi.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-public class TestApiSicredi {
+public class TestApi {
 
     @BeforeEach
     public void setUp() {
